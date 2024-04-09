35e25cff3a87de77995ec49337f3dd73d9456e13
ShikiBo(房士琦) <shiki.bo.f704@dirbato.co.jp>
2024-04-09 15:21:44 +0900

    テストコミット

---
+++ Test/src/calc/CalculatorTest.java
@@ -0,0 +1,23 @@
+package calc;
+
+import org.junit.Assert;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    @Test
+    void testAdd(){
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(4, calculator.add(2, 3));
+
+    }
+
+    @Test
+    void testSubtract(){
+        Calculator calculator = new Calculator();
+        assertEquals(-1, calculator.subtract(2, 3));
+    }
+
+}
